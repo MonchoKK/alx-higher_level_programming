d5f5ed0d8e2d213dd5b7f538be9138683003a084
Kabelo <kabelokamogelo96@gmail.com>
2024-03-20 19:12:40 -0700

    def roman_to_int(roman_string):     if not isinstance(roman_string, str) or roman_string is None:         return 0
        roman_numerals = {I: 1, V: 5, X: 10, L: 50,
                          C: 100, D: 500, M: 1000}
        result = 0
        prev_value = 0
    
        for char in reversed(roman_string):
            value = roman_numerals.get(char)
            if value is None:
                return 0
            if value < prev_value:
                result -= value
            else:
                result += value
            prev_value = value

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    number = 0
-    if type(roman_string) is not str or roman_string is None:
-        return (0)
-    for i, char in enumerate(roman_string):
-        temp = roman[char]
-        try:
-            if temp < roman[roman_string[i + 1]]:
-                temp = temp * -1
-            except IndexError:
-                pass
-            number = number + temp
-            return number
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_numerals.get(char)
+        if value is None:
+            return 0
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+        prev_value = value
+
+    return result
