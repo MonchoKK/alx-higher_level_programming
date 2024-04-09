3d3a5c76ab4700773d7b08fceab2c1ebbce927a0
Kabelo <kabelokamogelo96@gmail.com>
2024-04-09 09:29:42 -0700

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.n = getattr(madic_string, 'n', 0) + 1
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("BestSchool, " * (magic_string - 1) + "BestSchool")
