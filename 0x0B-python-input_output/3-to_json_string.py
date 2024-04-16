db04f6f7a68392b0707f64261677edda2c1bcec9
Kabelo <kabelokamogelo96@gmail.com>
2024-04-16 13:21:13 -0700

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,15 +7,3 @@ import json
 def to_json_string(my_obj):
     """Return the JSON representation of a string object."""
     return json.dumps(my_obj)
-
-0x0B-python-input_output/4-from_json_string.py
-
-#!/usr/bin/python3
-
-"""Defines a JSON-to-object function."""
-import json
-
-
-def from_json_string(my_str):
-    """Return the Python object representation of a JSON string."""
-    return json.loads(my_str)
