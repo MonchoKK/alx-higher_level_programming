4582c9e6c86ce56ba0751b0f0e0af180854b9091
Kabelo <kabelokamogelo96@gmail.com>
2024-05-23 02:16:55 -0700

    4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Create the table first_table in the current database
+
+-- Create table first_table if it does not already exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
