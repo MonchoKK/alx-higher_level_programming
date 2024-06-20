b5caeacb759f1ba610ed74b2cf7a13b3136bc98c
Kabelo <kabelokamogelo96@gmail.com>
2024-06-20 15:01:22 -0700

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,12 +5,22 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
+if (!fileA || !fileB || !fileC) {
+  console.log('Usage: ./concatFiles.js <first file> <second file> <destination file>');
+  process.exit(1);
+}
+
 try {
-  [fileA, fileB].forEach((f) => {
-    fs.readFileSync(f).toString().trim().split('\n').forEach((line) => {
-      fs.appendFileSync(fileC, line.toString() + '\n');
-    });
-  });
+  // Read content of first file
+  const contentA = fs.readFileSync(fileA, 'utf8');
+  // Read content of second file
+  const contentB = fs.readFileSync(fileB, 'utf8');
+
+  // Concatenate content and write to the destination file
+  fs.writeFileSync(fileC, contentA + contentB);
+
+  console.log('Files concatenated successfully.');
 } catch (err) {
-  console.log(err);
+  console.error('Error:', err.message);
+  process.exit(1);
 }
