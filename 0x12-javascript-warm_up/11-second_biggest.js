371ee7778769d85c7e842b04e6a6114857b1a6a8
Kabelo <kabelokamogelo96@gmail.com>
2024-06-20 13:46:11 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,9 +5,8 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const ints = [];
-  const len = process.argv.length - 2;
   for (let i = 2; i < process.argv.length; i++) {
     ints.push(parseInt(process.argv[i]));
   }
-  console.log(ints.sort()[len - 2]);
+  console.log(ints.sort()[1]);
 }
