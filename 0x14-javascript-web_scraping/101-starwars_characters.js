382694a5bcac19dfc1a8a4807a702c18b8ed67e8
Kabelo <kabelokamogelo96@gmail.com>
2024-07-31 09:21:28 -0700

    101-starwars_charcters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,32 @@
 #!/usr/bin/node
-/* Prints all the characters of a star wars movie
- * Takes args, first of which is the movie ID to request (GET) using the module 'request'
- * Displays oe character name per line & uses the Star Wars API
+/* Prints all the characters of a Star Wars movie
+ * Takes args, first of which is the movie ID to request (GET)
+ * Displays one character name per line & uses the Star Wars API
  */
 
 const request = require('request');
-if (process.argv.length > 2) {
-  request(`https://swapi-api.alx-tools.com/api/films/${parseInt(process.argv[2], 10)}`, function (error, response, body) {
-    if (error) console.log(error);
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error) console.log(error);
-        console.log(JSON.parse(body).name);
-      });
+const movieId = process.argv[2];
+
+if (movieId) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      return;
     }
+
+    const characters = JSON.parse(body).characters;
+    characters.forEach(character => {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        const name = JSON.parse(body).name;
+        console.log(name);
+      });
+    });
   });
+} else {
+  console.log('Usage: ./script.js <Movie ID>');
 }
